93aea2461ed58c0a53c179220582bf208202f217
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2022-11-11 04:49:02 +0600

    base.html template rendering

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'blog/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['title'] = "Home"
+        return context
--- core/settings/base.py
+++ core/settings/base.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 LOCALE_APPS = [
-
+    'apps.blog',
 ]
 
 THIRD_PARTY_APPS = [
